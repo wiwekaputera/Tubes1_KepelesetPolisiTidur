5af15735438a41e74cb9da5ac3709299a5af925d
I Made Wiweka Putera <78787384+wiwekaputera@users.noreply.github.com>
2025-03-15 12:26:21 +0700

    feat: complete minimum distance fire tracking

--- src/alternative-bots/KepelesetPolisiTidurAlt1/KepelesetPolisiTidurAlt1.cs
+++ src/alternative-bots/KepelesetPolisiTidurAlt1/KepelesetPolisiTidurAlt1.cs
@@ -4,49 +4,77 @@ using Robocode.TankRoyale.BotApi.Events;
 
 public class KepelesetPolisiTidurAlt1 : Bot
 {
-    // The main method starts our bot
+    // Variabel untuk melacak target terdekat
+    private double closestDistance = double.MaxValue;
+    private int closestBotId = -1;
+    // Variabel untuk menyimpan koordinat target terakhir yang terdeteksi
+    private double targetX = 0;
+    private double targetY = 0;
+
     static void Main()
     {
         new KepelesetPolisiTidurAlt1().Start();
     }
 
-    // Constructor, which loads the bot config file
-    KepelesetPolisiTidurAlt1() : base(BotInfo.FromFile("KepelesetPolisiTidurAlt1.json")) { }
+    public KepelesetPolisiTidurAlt1() : base(BotInfo.FromFile("KepelesetPolisiTidurAlt1.json")) { }
 
-    // Called when a new round is started -> initialize and do some movement
     public override void Run()
     {
-        BodyColor = Color.FromArgb(0xFF, 0x00, 0x00);   // Red
-        TurretColor = Color.FromArgb(0xFF, 0xFF, 0xFF); // White
-        RadarColor = Color.FromArgb(0xFF, 0x00, 0x00);  // Red
-        BulletColor = Color.FromArgb(0xFF, 0xFF, 0xFF); // White
-        ScanColor = Color.FromArgb(0xFF, 0x00, 0x00);   // Red
-        TracksColor = Color.FromArgb(0xFF, 0xFF, 0xFF); // White
-        GunColor = Color.FromArgb(0xFF, 0x00, 0x00);    // Red
+        BodyColor   = Color.FromArgb(0xFF, 0x20, 0x20, 0x20);
+        TurretColor = Color.FromArgb(0xFF, 0x40, 0x40, 0x40);
+        RadarColor  = Color.FromArgb(0xFF, 0x60, 0x60, 0x60);
+        GunColor    = Color.FromArgb(0xFF, 0x55, 0x55, 0x55);
+        TracksColor = Color.FromArgb(0xFF, 0x30, 0x30, 0x30);
+        BulletColor = Color.FromArgb(0xFF, 0xFF, 0x90, 0x00);
+        ScanColor   = Color.FromArgb(0xFF, 0xFF, 0x90, 0x00);
 
-        // Repeat while the bot is running
         while (IsRunning)
         {
-            Forward(100);
-            TurnGunRight(360);
-            Back(100);
-            TurnGunRight(360);
+            if (closestBotId != -1)
+            {
+                double angleToTarget = BearingTo(targetX, targetY);
+                double turnAngle = NormalizeRelativeAngle(angleToTarget - Direction);
+                TurnRight(turnAngle);
+
+                // TODO: Greedy movement
+                // TODO: TurnGun sebanyak selisih ke closestBotId
+                Forward(100);
+                TurnGunRight(360);
+                Back(100);
+                TurnGunRight(360);
+            }
+            else
+            {
+                TurnGunRight(360);
+            }
         }
     }
 
-    // We saw another bot -> fire!
     public override void OnScannedBot(ScannedBotEvent evt)
     {
-        Fire(1);
+        double dist = DistanceTo(evt.X, evt.Y);
+        if (dist < closestDistance)
+        {
+            closestDistance = dist;
+            closestBotId = evt.ScannedBotId;
+            targetX = evt.X;
+            targetY = evt.Y;
+        }
+        
+        if (evt.ScannedBotId == closestBotId)
+        {
+            Fire(1);
+        } else {
+            Fire(1);
+        }
     }
 
-    // We were hit by a bullet -> turn perpendicular to the bullet
-    public override void OnHitByBullet(HitByBulletEvent evt)
+    public override void OnBotDeath(BotDeathEvent evt)
     {
-        // Calculate the bearing to the direction of the bullet
-        double bearing = CalcBearing(evt.Bullet.Direction);
-
-        // Turn 90 degrees to the bullet direction based on the bearing
-        TurnLeft(90 - bearing);
+        if (evt.VictimId == closestBotId)
+        {
+            closestDistance = double.MaxValue;
+            closestBotId = -1;
+        }
     }
 }
