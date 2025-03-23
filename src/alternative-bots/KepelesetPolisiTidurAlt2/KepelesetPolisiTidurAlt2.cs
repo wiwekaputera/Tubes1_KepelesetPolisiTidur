2361cf63ca02d098c1bf2eed3acc56a92c4fba59
unknown <naufarrelzhafifabhista@gmail.com>
2025-03-23 14:40:27 +0700

    refactor: changed alt bot 2 from first-scanned to least-energy

--- src/alternative-bots/KepelesetPolisiTidurAlt2/KepelesetPolisiTidurAlt2.cs
+++ src/alternative-bots/KepelesetPolisiTidurAlt2/KepelesetPolisiTidurAlt2.cs
@@ -2,81 +2,104 @@ using System.Drawing;
 using Robocode.TankRoyale.BotApi;
 using Robocode.TankRoyale.BotApi.Events;
 
+/*
+ * KepelesetPolisiTidurAlt2
+ * 
+ * Strategy:
+ * - Greedy Least-Energy: selalu target bot dengan energi paling rendah
+ * - Gun muter 360 untuk scanning + fire kalau musuh yang discan = target
+ */
+
 public class KepelesetPolisiTidurAlt2 : Bot
 {
     private int? targetId = null;
-    private int lostTargetCounter = 0;
+    private double targetX = 0;
+    private double targetY = 0;
+    private double targetEnergy = double.MaxValue;
 
-    static void Main()
-    {
-        new KepelesetPolisiTidurAlt2().Start();
-    }
+    private bool rotateGunRight = true;
 
-    KepelesetPolisiTidurAlt2() : base(BotInfo.FromFile("KepelesetPolisiTidurAlt2.json")) { }
+    static void Main() => new KepelesetPolisiTidurAlt2().Start();
+    public KepelesetPolisiTidurAlt2() : base(BotInfo.FromFile("KepelesetPolisiTidurAlt2.json")) { }
 
     public override void Run()
     {
-        // Set warna biar keren
-        BodyColor = Color.Red;
-        TurretColor = Color.White;
-        RadarColor = Color.Red;
-        BulletColor = Color.White;
-        ScanColor = Color.Red;
-        TracksColor = Color.White;
-        GunColor = Color.Red;
+        BodyColor   = Color.DarkRed;
+        TurretColor = Color.Red;
+        RadarColor  = Color.OrangeRed;
+        GunColor    = Color.Firebrick;
+        TracksColor = Color.DimGray;
+        BulletColor = Color.Yellow;
+        ScanColor   = Color.Orange;
 
-        // Gerak awal biar ga diem aja
-        Forward(100);
-        TurnRight(90);
-
-        // Loop utama
         while (IsRunning)
         {
-            if (targetId == null)
+            if (targetId != null)
             {
-                TurnGunRight(10);  // Kalau belum ada target, scanning dulu
-            }
-            else
-            {
-                // Bergerak ke arah target sambil tembak
-                Forward(50);
-                Fire(3);
+                // Putar body ke arah target
+                double turnAngle = NormalizeRelativeAngle(BearingTo(targetX, targetY));
+                TurnRight(turnAngle);
 
-                // Kalau target ilang lebih dari 30 tick, reset
-                lostTargetCounter++;
-                if (lostTargetCounter > 30)
-                {
-                    targetId = null;
-                    lostTargetCounter = 0;
-                }
+                // Gerak tergantung jarak
+                double dist = DistanceTo(targetX, targetY);
+                if (dist > 150)
+                    Forward(100);
+                else if (dist < 50)
+                    Back(100);
+                else
+                    Forward(50);
             }
+
+            if (rotateGunRight)
+                TurnGunRight(360);
+            else
+                TurnGunLeft(360);
+
+            rotateGunRight = !rotateGunRight;
         }
     }
 
-   public override void OnScannedBot(ScannedBotEvent evt)
+    public override void OnScannedBot(ScannedBotEvent evt)
     {
-        if (targetId == null)
+        // Selalu update ke musuh dengan energi paling rendah
+        if (evt.Energy < targetEnergy)
         {
-            targetId = evt.ScannedBotId;  // Kunci target pertama yang terlihat
+            targetId = evt.ScannedBotId;
+            targetX = evt.X;
+            targetY = evt.Y;
+            targetEnergy = evt.Energy;
         }
-        else if (targetId == evt.ScannedBotId)
+
+        if (evt.ScannedBotId == targetId)
         {
-            // Dapatkan sudut turret ke target
-            double gunBearing = GunBearingTo(evt.X, evt.Y);
-
-            // Putar turret ke arah target
-            TurnGunLeft(gunBearing);
-
-            // Tembak full power
             Fire(3);
-            lostTargetCounter = 0;  // Reset lost counter kalau kita masih melihat target
+        }
+    }
 
-            // Jika musuh jauh (> 100), mendekat
-            double distance = DistanceTo(evt.X, evt.Y);
-            if (distance > 100)
-            {
-                Forward(50);
-            }
+    public override void OnTick(TickEvent evt)
+    {
+        // Reset energi terendah agar selalu cari yang terbaru tiap tick
+        targetEnergy = double.MaxValue;
+    }
+
+    public override void OnHitWall(HitWallEvent evt)
+    {
+        Back(50);
+        TurnRight(90);
+    }
+
+    public override void OnHitBot(HitBotEvent evt)
+    {
+        Back(100);
+        TurnRight(45);
+    }
+
+    public override void OnBotDeath(BotDeathEvent evt)
+    {
+        if (evt.VictimId == targetId)
+        {
+            targetId = null;
+            targetEnergy = double.MaxValue;
         }
     }
 }
